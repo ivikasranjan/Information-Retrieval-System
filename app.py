6b2e032308351316dfb6fd1536ab37ad76a585e4
ivikasranjan <ivikasranjan@outlook.com>
2025-08-25 20:51:48 +0530

    completed

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+
+
+def main():
+    st.set_page_config("Information Retrieval")
+    st.header("Information Retrieval System💁")
+    
+    
+    
+    
+    if __name__ == "__main__":
+        main()
